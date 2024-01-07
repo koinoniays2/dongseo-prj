a84ca9d40d285943b3543c162f883d51a8343d82
koinoniays2 <koinonia_ys@naver.com>
2024-01-07 22:07:12 +0900

    test-4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    // --------------------섹션-1 텍스트--------------------
+// --------------------섹션-1 텍스트--------------------
     let textP = document.querySelector(".text-box > p");
     let textSpan = document.querySelector(".text-box > span");
     let section = document.querySelector("#section-1");
@@ -21,7 +21,7 @@ window.addEventListener("load", function () {
         };
     });
 
-    // --------------------섹션-2--------------------
+// --------------------섹션-2--------------------
     gsap.registerPlugin(ScrollTrigger);
     // 라인 애니메이션
     gsap.to(".sassang", {
@@ -37,16 +37,16 @@ window.addEventListener("load", function () {
     let tl = gsap.timeline();
     tl.fromTo(".text-box-2", {
         opacity: 0,
-        x: -1000
+        y: 1000
     }, {
         opacity: 1,
-        x: 0,
+        y: 0,
         duration: 1
-    }, "+=0.5")
-    // 밑줄 라인
+    })
+        // 밑줄 라인
     .to(".border-line", {
         width: "90%"
-    }, "-=0.25");
+    }, "-=0.3");
     // 스크롤 트리거 설정
     ScrollTrigger.create({
         trigger: "#section-2",
@@ -55,23 +55,18 @@ window.addEventListener("load", function () {
         animation: tl, // 위에서 만든 타임라인을 설정
         scrub: 5
     });
-    // --------------------섹션-3--------------------
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: "#section-3",
-            start: "top center",
-            end: "center center",
-            scrub: 1
-        }
-    });
+// --------------------섹션-3--------------------
+    let tl2 = gsap.timeline();
+    // 이미지 애니메이션
     let clippingImg = document.querySelectorAll(".img-box-2 > img");
     clippingImg.forEach((item) => {
         tl2.fromTo(item, {
-            opacity: 0,
+            opacity: 0
         }, {
             opacity: 1
         })
     });
+    // 텍스트 애니메이션
     let text3 = document.querySelectorAll(".text-box-3 > p");
     text3.forEach((item) => {
         tl2.fromTo(item, {
@@ -79,5 +74,13 @@ window.addEventListener("load", function () {
         }, {
             opacity: 1
         })
+    });
+    // 스크롤 트리거 설정
+    ScrollTrigger.create({
+        trigger: "#section-3",
+        start: "top center",
+        end: "center center",
+        animation: tl2, // 위에서 만든 타임라인을 설정
+        scrub: 5
     });
 });
